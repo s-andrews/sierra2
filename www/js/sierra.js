cfa0c10511003e9babee137b5d814e8560602808
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-07-30 12:31:40 +0100

    Started submission list layout

---
+++ www/js/sierra.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+    set_up_submissions()
+});
+
+
+function set_up_submissions() {
+
+    // This toggles the submission details in the list of submissions
+    $(".showsubmissiondetails").click(function(e) {
+        e.preventDefault()
+        $(this).parent().nextAll(".submissiondetails").toggle()
+    }) 
+
+
+    // This toggles the biosample details in the list of submissions
+    $(".showbiosamples").click(function(e) {
+        e.preventDefault()
+        $(this).parent().nextAll(".biosamples").toggle()
+    }) 
+    
+}
